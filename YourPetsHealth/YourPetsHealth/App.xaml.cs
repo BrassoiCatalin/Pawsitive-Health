82af428a540e2d6c911d602b5195dc67fca25021
Catalin Brassoi <cbrassoi@talentingsoftware.com>
2023-02-20 23:15:39 +0200

    Established connection to Firebase database.

--- YourPetsHealth/YourPetsHealth/App.xaml.cs
+++ YourPetsHealth/YourPetsHealth/App.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Firebase.Database.Query;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using YourPetsHealth.Models;
 using YourPetsHealth.Views;
 
 namespace YourPetsHealth
@@ -10,9 +15,34 @@ namespace YourPetsHealth
         public App()
         {
             InitializeComponent();
+            InitializeAsync();
+
 
             //MainPage = new AppShell();
             MainPage = new LoginView();
+        }
+
+        private async Task InitializeAsync()
+        {
+            var firebaseClient = new Firebase.Database.FirebaseClient("https://your-pets-health-76f3b-default-rtdb.europe-west1.firebasedatabase.app/");
+            try
+            {
+                //await firebaseClient.Child(nameof(User)).Child("4").PutAsync(new User()
+                //{
+                //    Id = 4,
+                //    FirstName = "Test",
+                //    LastName = "Test"
+                //});
+
+                var userList = (await firebaseClient.Child(nameof(User)).OnceAsListAsync<User>()).Where(x => x.Object != null).Select(x => x.Object).ToList();
+                var xx = 3;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+
+
         }
 
         protected override void OnStart()
